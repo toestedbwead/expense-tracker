02c3f8e4efc16ac8196a6dbe0777984402ae1aa1
toestedbwead <leielcano@gmail.com>
2025-11-26 15:06:17 +0800

    Defined commands for the Expense Tracker CLI

--- expense-tracker.py
+++ expense-tracker.py
@@ -2,6 +2,7 @@ import sys
 import json
 import os
 from datetime import datetime
+import argparse
 
 EXPENSES_FILE = 'expenses-tracker.json'
 
@@ -24,11 +25,37 @@ def save_expenses(expenses):
         print(f"Error saving tasks: {e}")
 
 def main():
-    if len(sys.argv) > 6:
-        print("Usage: python expense-tracker.py <command> [args] ")
-        return
-    
-    
+   parser = argparse.ArgumentParser(description='Expense Tracker CLI')
+
+   subparsers = parser.add_subparsers(dest='command', help='Available commands')
+
+   # add command
+   add_parser = subparsers.add_parser('add', help='Add a new expense')
+   add_parser.add_argument('--description', required=True, help='Expense description')
+   add_parser.add_argument('--amount', type=float, required=True, help='Expense amount')
+
+   # delete command
+   delete_parser = subparsers.add_parser('delete', help='Delete an expense')
+   delete_parser.add_argument('--id', type=int, required=True, help='Expense ID to delete')
+
+   # update command
+   update_parser = subparsers.add_parser('update', help='Update an expense')
+   update_parser.add_argument('--id', type=int,required=True, help='Expense ID to update')
+   update_parser.add_argument('--description', help='Description to be updated')
+   update_parser.add_argument('--amount', type=float, help='Amount to be updated')
+
+   # list command
+   list_parser = subparsers.add_parser('list', help='List of expenses')
+
+   # summary of all expenses command
+   summary_parser = subparsers.add_parser('summary', help="Summary of all expenses")
+
+   summary_parser.add_argument('--month', type=int,help='Summary of expenses per specific month. 1-12')
+
+   args = parser.parse_args()
+   
+
+
 
 
 
